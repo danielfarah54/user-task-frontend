14503ba057c107f9c27cdd8b098b45df3ef9ccef
Daniel Farah <danielfarah54@gmail.com>
2021-09-17 14:54:16 -0300

    refactor graphql service

--- src/app/graphql/graphql.service.ts
+++ src/app/graphql/graphql.service.ts
@@ -1,8 +1,10 @@
+import { setContext } from '@apollo/client/link/context';
 import { HttpLink } from 'apollo-angular/http';
 import { Apollo } from 'apollo-angular';
 import { Injectable } from '@angular/core';
-import { InMemoryCache, ApolloLink, concat } from '@apollo/client/core';
-import { HttpHeaders } from '@angular/common/http';
+import { InMemoryCache, ApolloLink } from '@apollo/client/core';
+
+const uri = 'http://localhost:4000/graphql';
 
 @Injectable({
   providedIn: 'root',
@@ -11,21 +13,32 @@ export class GraphqlService {
   constructor(private apollo: Apollo, private httpLink: HttpLink) {}
 
   createApollo() {
-    const http = this.httpLink.create({ uri: 'http://localhost:4000/graphql' });
-    const token = localStorage.getItem('id_token');
-    const authLink = new ApolloLink((operation, forward) => {
-      operation.setContext({
-        headers: new HttpHeaders().set(
-          'Authorization',
-          token ? `bearer ${token}` : ''
-        ),
-      });
-      return forward(operation);
+    const basic = setContext((operation, context) => ({
+      headers: {
+        Accept: 'charset=utf-8',
+      },
+    }));
+
+    const auth = setContext((operation, context) => {
+      const token = localStorage.getItem('id_token');
+
+      if (token === null) {
+        return {};
+      } else {
+        return {
+          headers: {
+            Authorization: `bearer ${token}`,
+          },
+        };
+      }
     });
 
+    const link = ApolloLink.from([basic, auth, this.httpLink.create({ uri })]);
+    const cache = new InMemoryCache();
+
     this.apollo.create({
-      link: concat(authLink, http),
-      cache: new InMemoryCache(),
+      link,
+      cache,
     });
   }
 }
